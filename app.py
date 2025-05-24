7473470e8f15b7e0e526a9b27621039b06d9e6b0
dhanuvagman006 <dhnaushinsit@gmail.com>
2025-05-24 18:40:26 +0530

    removed unnessary

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import *
 import airedirect
 app = Flask(__name__)
-
-api_key='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'
+api_key='<add your api key here>'
 model="google/gemini-2.0-flash-001"
 
 
